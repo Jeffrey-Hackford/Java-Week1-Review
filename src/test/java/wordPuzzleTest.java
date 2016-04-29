fb53ed0016f5bd86da0d2eec38bd6e9df5dcc071
Jeffrey Hackford <jeffrey.hackford@gmail.com>
2016-04-28 19:20:38 -0700

    add unit test to handle a user inputted 'space'

--- src/test/java/wordPuzzleTest.java
+++ src/test/java/wordPuzzleTest.java
@@ -4,19 +4,29 @@ import static org.junit.Assert.*;
 public class wordPuzzleTest {
 
   @Test
-  public void isWordPuzzleTestA_makeWordPuzzle_a() {
+  public void isWordPuzzleTestA_makeWordPuzzle_StringA() {
     wordPuzzle testWordPuzzle = new wordPuzzle();
     assertEquals("-", testWordPuzzle.makeWordPuzzle("a"));
   }
   @Test
-  public void isWordPuzzleTestI_makeWordPuzzle_i() {
+  public void isWordPuzzleTestI_makeWordPuzzle_StringI() {
     wordPuzzle testWordPuzzle = new wordPuzzle();
     assertEquals("-", testWordPuzzle.makeWordPuzzle("i"));
   }
   @Test
-  public void isWordPuzzleTestB_makeWordPuzzle_b() {
+  public void isWordPuzzleTestB_makeWordPuzzle_StringB() {
     wordPuzzle testWordPuzzle = new wordPuzzle();
-    assertEquals(" ", testWordPuzzle.makeWordPuzzle("b"));
+    assertEquals("b", testWordPuzzle.makeWordPuzzle("b"));
+  }
+  @Test
+  public void isWordPuzzleTestSpace_makeWordPuzzle_StringSpace() {
+    wordPuzzle testWordPuzzle = new wordPuzzle();
+    assertEquals(" ", testWordPuzzle.makeWordPuzzle(" "));
+  }
+  @Test
+  public void isWordPuzzleTestB_makeWordPuzzle_StringLowerCaseM() {
+    wordPuzzle testWordPuzzle = new wordPuzzle();
+    assertEquals("M", testWordPuzzle.makeWordPuzzle("m"));
   }
   @Test
   public void isWordPuzzleTestWord_makeWordPuzzle_word() {
